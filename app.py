ae7a090b640abec1947a7c67abef58985c99ef90
Jakub Wojciechowski <jakubwojciechowski@users.noreply.github.com>
2019-11-02 14:32:22 +0100

    Registration MYSQL working

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 
 
 #___MyDQL config___
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '123456'
-app.config['MYSQL_DB'] = 'hermes_web_aplication'
+app.config['MYSQL_HOST'] = 'sql7.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql7310443'
+app.config['MYSQL_PASSWORD'] = 'zIpZ79LfA5'
+app.config['MYSQL_DB'] = 'sql7310443'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 #___MyDQL config___
 mysql = MySQL(app)
@@ -23,12 +23,15 @@ def index():
 
 
 #___Login block___
-
 @app.route('/log', methods=['GET', 'POST'])
 def log():
     if request.method == 'POST':
+
+        """
         username = request.form['username']
         password_candidate = request.form['password']
+        
+        
         #___Create cursor___
         cur = mysql.connection.cursor()
         #___Get User by Username___
@@ -38,12 +41,13 @@ def log():
             data = cur.fetchone()
             password = data['password']
 
-            if sha256_crypt.verify(password_candidate, password):
-                app.logger.info('PASSWORD MATCHED')
+            #if sha256_crypt.verify(password_candidate, password):
+                #app.logger.info('PASSWORD MATCHED')
 
-        else:
-            app.logger.info('USER NOT FOUND')
+        #else:
 
+            #app.logger.info('USER NOT FOUND')
+        """
         return redirect(url_for('index'))
 
     return render_template('log_wtf.html')
@@ -72,13 +76,13 @@ def register():
         #___Create cursor___
         cur = mysql.connection.cursor()
         #___SQL Query___
-        cur.execute("INSERT INTO Users(_name_, UserEmail, UserName, UserPassword) VALUES(%s, %s, %s, %s)",(name, email, username, password))
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",(name, email, username, password))
         #___Commit to DB___
         mysql.connection.commit()
         #___Closing connection___
         cur.close()
 
-        return redirect(url_for('log_wtf'))
+        return redirect(url_for('index'))
 
     return render_template('register_wtf.html', form = form)
 
